b6370df4a346fa939568d2743fcc6f7003b34a86
Andrey Shklyaruk <13mailg@gmail.com>
2023-11-24 16:09:56 +0200

    set CORS in response

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -19,7 +19,15 @@ export async function POST(request: Request) {
         ON CONFLICT (id) DO NOTHING;
       `
 
-    return NextResponse.json({ message: "CRESTED SUBSCRIPTION", success: true, subscription });
+    // return NextResponse.json({ message: "CRESTED SUBSCRIPTION", success: true, subscription });
+    return new Response(JSON.stringify({ message: "CRESTED SUBSCRIPTION", success: true, subscription }), {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
   } catch (err) {
     console.log(err);
     return NextResponse.json({ message: err, success: false });

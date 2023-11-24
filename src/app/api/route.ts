9d2fc182c81e58f8d70a9ccc70ae920bde69f14d
Andrey Shklyaruk <13mailg@gmail.com>
2023-11-24 22:43:54 +0200

    upd

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,16 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { createKysely } from '@vercel/postgres-kysely';
 import { sql } from '@vercel/postgres';
 import { Database } from '../../../db/model';
 
 const db = createKysely<Database>();
+const webpush = require('web-push');
 
 export async function POST(request: Request) {
   try {
     const subscription = await request.json();
 
     if (!subscription.endpoint) {
-      throw "Subscription must have the endpoint!";
+      throw "Subscription must have endpoint.";
     }
 
     await sql`
@@ -20,25 +21,53 @@ export async function POST(request: Request) {
       `
 
     return NextResponse.json({ message: "CRESTED SUBSCRIPTION", success: true, subscription });
-    // return new Response(JSON.stringify({ message: "CRESTED SUBSCRIPTION", success: true, subscription }), {
-    //   status: 200,
-    //   headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    //     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    //   },
-    // });
   } catch (err) {
     console.log(err);
     return NextResponse.json({ message: err, success: false });
   }
 }
 
-export async function GET() {
-  const subscriptions = await db
-    .selectFrom('subscriptions')
-    .selectAll()
-    .execute()
+export async function GET(request: NextRequest) {
+  try {
+    const message = 'NO MESSAGE';
+    console.log('REQUEST: ', request);
 
-  return NextResponse.json({ success: true, subscriptions: subscriptions.length });
+    const subscriptions = await db.selectFrom('subscriptions').selectAll().execute()
+
+    for (let i = 0; i < subscriptions.length; i++) {
+      await triggerNotification(subscriptions[i], message)
+    }
+
+    return NextResponse.json({ success: true, recipients: subscriptions.length, message });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: err, success: false });
+  }
 }
+
+async function triggerNotification(subscription: any, message: string) {
+  return webpush.sendNotification(subscription, JSON.stringify({
+    notification: {
+      title: message,
+      data: {
+        onActionClick: {
+          default: {
+            operation: "openWindow",
+            url: "http://localhost:3000/"
+          }
+        }
+      }
+    }
+  }), {
+    vapidDetails: {
+      subject: 'mailto:email@example.com',
+      publicKey: process.env.PUBLIC_KEY,
+      privateKey: process.env.PRIVATE_KEY
+    },
+    TTL: 60
+  })
+    .catch((err: any) => {
+      console.error("Error sending notification, reason: ", err);
+    });
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1>WEB-push app</h1>
+    <h1>WEB-push app updated</h1>
   )
 }

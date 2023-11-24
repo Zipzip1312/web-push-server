c5df293ec185c1f039be66d33f7458b5d60adcee
Andrey Shklyaruk <13mailg@gmail.com>
2023-11-24 22:37:35 +0200

    api route default handler upd

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,14 +1,44 @@
 import { NextResponse } from "next/server";
+import { createKysely } from '@vercel/postgres-kysely';
+import { sql } from '@vercel/postgres';
+import { Database } from '../../../db/model';
 
-export async function GET(request: Request) {
-  return NextResponse.json({ message: "GET This Worked", success: true });
-}
+const db = createKysely<Database>();
 
 export async function POST(request: Request) {
   try {
-    return NextResponse.json({ message: "POST This Worked", success: true });
+    const subscription = await request.json();
+
+    if (!subscription.endpoint) {
+      throw "Subscription must have the endpoint!";
+    }
+
+    await sql`
+        INSERT INTO subscriptions (endpoint, expirationTime, keys)
+        VALUES (${subscription.endpoint}, ${subscription.expirationTime}, ${subscription.keys})
+        ON CONFLICT (id) DO NOTHING;
+      `
+
+    return NextResponse.json({ message: "CRESTED SUBSCRIPTION", success: true, subscription });
+    // return new Response(JSON.stringify({ message: "CRESTED SUBSCRIPTION", success: true, subscription }), {
+    //   status: 200,
+    //   headers: {
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    //     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    //   },
+    // });
   } catch (err) {
     console.log(err);
     return NextResponse.json({ message: err, success: false });
   }
+}
+
+export async function GET() {
+  const subscriptions = await db
+    .selectFrom('subscriptions')
+    .selectAll()
+    .execute()
+
+  return NextResponse.json({ success: true, subscriptions: subscriptions.length });
 }

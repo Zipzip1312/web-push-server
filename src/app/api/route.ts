e7ade6d95b85c1f7db36313e1d3b9b929268144d
Andrey Shklyaruk <13mailg@gmail.com>
2023-11-24 20:43:06 +0200

    enable cors

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!', {
-    status: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-  })
-  // return NextResponse.json({ message: "GET This Worked", success: true });
+  return NextResponse.json({ message: "GET This Worked", success: true });
 }
 
 export async function POST(request: Request) {

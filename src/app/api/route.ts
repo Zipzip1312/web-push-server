0ad523e2ad7c67cf3d14533f307f0348ce8a76ba
Andrey Shklyaruk <13mailg@gmail.com>
2023-11-24 16:26:19 +0200

    test

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  return NextResponse.json({ message: "GET This Worked", success: true });
+  return new Response('Hello, Next.js!', {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
+  // return NextResponse.json({ message: "GET This Worked", success: true });
 }
 
 export async function POST(request: Request) {

5d5ba79ed837052cafb0415ed1ed457a488edeaa
Andrey Shklyaruk <13mailg@gmail.com>
2023-12-07 12:31:23 +0200

    another vars naming fixed

--- db/placeholder-data.js
+++ db/placeholder-data.js
@@ -3,8 +3,8 @@
 
 const subscriptions = [
   {
-    userId: 1,
-    userName: 'Test User 22',
+    userid: 1,
+    username: 'Test User 22',
     endpoint: "https://fcm.googleapis.com/fcm/send/dRJcqrv-rmM:APA91bFO_7G73kxzDixQhbgio3An2rbEUQofZbwz27tJM7K1aP3ZxOqV7hnIWc2ftaxScZtzZMVfTnV500FgwySwfIketG7YEn13vzxrrb0WzuhJxBfEVtBLZTAIOuRFA_wez3yxP__Q",
     expirationTime: null,
     keys: {
